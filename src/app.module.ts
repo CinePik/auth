750503d224ce3bef2d2e9430749445853260159f
mevljas <sebastjan.mevlja@telekom.si>
2023-12-03 01:42:31 +0100

    initial keycloak setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+
 import { UsersModule } from './users/users.module';
+import { KeycloakModule } from './keycloak/keycloak.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [UsersModule, KeycloakModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/keycloak/keycloak.module.ts
@@ -0,0 +1,85 @@
+import { Module } from '@nestjs/common';
+
+import { AppController } from '../app.controller';
+
+import { AppService } from '../app.service';
+
+import {
+  KeycloakConnectModule,
+  ResourceGuard,
+  RoleGuard,
+  AuthGuard,
+} from 'nest-keycloak-connect';
+
+import { APP_GUARD } from '@nestjs/core';
+
+@Module({
+  imports: [
+    KeycloakConnectModule.register({
+      authServerUrl: 'http://localhost:8080',
+
+      realm: 'cinepik',
+
+      clientId: 'nest-auth',
+
+      secret: 'kDTL1eSxPIFR8Ai0UXjvzYkaZVpy2M6c',
+
+      // Secret key of the client taken from keycloak server
+    }),
+  ],
+
+  controllers: [AppController],
+
+  providers: [
+    AppService,
+
+    // This adds a global level authentication guard,
+
+    // you can also have it scoped
+
+    // if you like.
+
+    //
+
+    // Will return a 401 unauthorized when it is unable to
+
+    // verify the JWT token or Bearer header is missing.
+
+    {
+      provide: APP_GUARD,
+
+      useClass: AuthGuard,
+    },
+
+    // This adds a global level resource guard, which is permissive.
+
+    // Only controllers annotated with @Resource and
+
+    // methods with @Scopes
+
+    // are handled by this guard.
+
+    {
+      provide: APP_GUARD,
+
+      useClass: ResourceGuard,
+    },
+
+    // New in 1.1.0
+
+    // This adds a global level role guard, which is permissive.
+
+    // Used by `@Roles` decorator with the
+
+    // optional `@AllowAnyRole` decorator for allowing any
+
+    // specified role passed.
+
+    {
+      provide: APP_GUARD,
+
+      useClass: RoleGuard,
+    },
+  ],
+})
+export class KeycloakModule {}
