70cf9f063f5fbecb17af697ea17ab68d755661e0
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2023-12-04 20:57:07 +0100

    use process.env for node port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService: ConfigService = app.get(ConfigService);
-  await app.listen(configService.get('NODE_PORT'));
+  await app.listen(process.env.NODE_PORT);
 }
 bootstrap();
