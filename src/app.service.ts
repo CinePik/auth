9df657c0dee6c373bf7bcbf1fda605b28dc9477a
mevljas <sebastjan.mevlja@telekom.si>
2023-12-26 21:48:25 +0100

    add project version to hello world endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  info(): string {
+    return 'CinePik auth v' + process.env.npm_package_version;
   }
 }
