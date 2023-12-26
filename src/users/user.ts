cab00650a8d9e8a13d7df8abaf5b8e356ac8b417
mevljas <sebastjan.mevlja@telekom.si>
2023-12-26 19:51:27 +0100

    remove users module

--- src/users/user.ts
+++
@@ -1,6 +0,0 @@
-export class User {
-  userId: number;
-  username: string;
-  // TODO: example only; do NOT store passwords in plain text in production, use bcrypt (one way hash with salt)
-  password: string;
-}
